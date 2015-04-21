666e07e0daeb4e361e8d880a7444a9ff5cacbb3e
Arthur Jamain <arthur.jamain@gmail.com>
2015-04-21 19:06:35 +0200

    renamed build script

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 
+rm -r dist
+mkdir dist
+git archive --format zip --output dist/sketchfab.zip master
--- dist.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir dist
-git archive --format zip --output dist/sketchfab.zip master
