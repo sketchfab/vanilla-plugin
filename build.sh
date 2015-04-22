81f4c5ca9036c999b6be73a691167a383c07327e
Arthur Jamain <arthur.jamain@gmail.com>
2015-04-22 10:19:08 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -2,4 +2,14 @@
 
 rm -r dist
 mkdir dist
-git archive --format zip --output dist/sketchfab.zip master
+mkdir dist/Sketchfab
+
+cp class.sketchfab.plugin.php dist/Sketchfab/
+cp icon.png dist/Sketchfab/
+cp -r js dist/Sketchfab/
+cp -r design dist/Sketchfab/
+cp -r views dist/Sketchfab/
+
+cd dist
+zip -r sketchfab-plugin.zip *
+rm -r Sketchfab
