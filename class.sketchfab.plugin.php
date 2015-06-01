14414a8c50e49c287cbdb1c80f3c276862184fcb
lincolnwebs <lincolnwebs@gmail.com>
2015-06-01 15:48:27 -0400

    Move CSS add to AssetModel and reformat docs

--- class.sketchfab.plugin.php
+++ class.sketchfab.plugin.php
@@ -14,22 +14,31 @@ $PluginInfo['Sketchfab'] = array(
 class SketchfabPlugin extends Gdn_Plugin {
 
  /**
-  * Base_Render_Before Event Hook
+  * Add a Javascript file to every page.
   *
-  * This is a common hook that fires for all controllers (Base), on the Render method (Render), just 
-  * before execution of that method (Before). It is a good place to put UI stuff like CSS and Javascript 
-  * inclusions. Note that all the Controller logic has already been run at this point.
-  *
-  * @param $Sender Sending controller instance
+  * @param $Sender Gdn_Controller Sending controller instance.
   */
   public function Base_Render_Before($Sender) {
-    $Sender->AddCssFile($this->GetResource('design/sketchfab.css', FALSE, FALSE));
-    $Sender->AddJsFile($this->GetResource('js/sketchfab.js', FALSE, FALSE));
+    $Sender->AddJsFile($this->GetResource('js/sketchfab.js', false, false));
   }
+
   /**
-   * Custom hook, emitted by the Advanced Editor plugin. This makes the Advanced Editor a dependency of this plugin.
+   * Add CSS file to asset handler.
+   *
+   * @param $Sender AssetModel Where we add the CSS file.
    */
-  public function EditorPlugin_InitEditorToolbar_Handler($Sender, $args) {
+  public function AssetModel_StyleCss_Handler($Sender) {
+    $Sender->AddCssFile($this->GetResource('design/sketchfab.css', false, false));
+  }
+
+  /**
+   * Add Sketchfab button to the editor's toolbar.
+   *
+   * Custom hook, emitted by the Advanced Editor plugin. This makes the Advanced Editor a dependency of this plugin.
+   *
+   * @param $Sender EditorPlugin Not used but required.
+   */
+  public function EditorPlugin_InitEditorToolbar_Handler($Sender) {
     echo '<span id="embed-sketchfab-plugin-button" class="editor-action icon editor-action-sketchfab">S</span>';
   }
 
